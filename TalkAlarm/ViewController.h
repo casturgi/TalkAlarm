534b3a4bc6ca940263960a4fcf3ec95f1849f7d7
Cory Sturgis-Casturgi <casturgi@gmail.com>
2016-02-08 21:47:46 -0600

    Initial Commit

---
+++ TalkAlarm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TalkAlarm
+//
+//  Created by cory Sturgis on 2/8/16.
+//  Copyright © 2016 CorySturgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TalkAlarm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TalkAlarm
+//
+//  Created by cory Sturgis on 2/8/16.
+//  Copyright © 2016 CorySturgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
